cf4574f7ccb1684e3c336e719954f7dabcabc98a
AleHutmacher <91856761+AleHutmacher@users.noreply.github.com>
2023-11-05 20:31:29 -0300

    Agregando timer visualmente al UI

--- Assets/Finished Scripts/MinijuegoHormiga/RamaBehaviour.cs
+++ Assets/Finished Scripts/MinijuegoHormiga/RamaBehaviour.cs
@@ -8,8 +8,7 @@ public class RamaBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // cambiamos el punto de pivot
-        GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
     }
 
     // Update is called once per frame
